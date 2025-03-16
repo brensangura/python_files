a8c3ed52aa9d90b35fa5b471c96764d6543b6d70
brensangura <sangura.bren>
2025-03-16 06:09:58 +0300

    this was done by sangura at 5am

---
+++ lesson3.py
@@ -0,0 +1,23 @@
+print ("hello world, this is session 3")
+
+number = 12
+number2 = 12.7
+
+name = ("Evans Mutuku")  # Define the variable 'name'
+dev = True
+print(name)
+
+print ("keep calm sangura")
+
+print(number)    # Now this will work
+
+students = ["Margaret", "Fidel", "John", "Lyza", "Derrick", 5, True]
+print(students)
+print(students[0])
+
+number = 12
+number2 = 12.7
+
+name = "Evans Mutuku"  # Define the variable 'name'
+dev = True
+print(name)  # Print the value of 'name'
---
+++ youtube tutorial.py
@@ -0,0 +1,23 @@
+print("Hello World")
+print("python 3 is awesome")
+
+print(5)
+print(34.5)
+
+city = "Kathmandu"
+print(city)
+
+city = "New York"
+print(city)
+
+city = destination_city
+print(city)
+
+my_fav_number = 5
+print(my_fav_number)
+
+pi = 3.14
+print(pi)
+
+my_fav_number =pi
+print(my_fav_number)
