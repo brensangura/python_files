760a5d7b6ffb4c350a0218ec0f94f452ddc21f07
brensangura <sangura.bren>
2025-03-16 06:17:30 +0300

    I am through with lesson2

---
+++ lesson2.py
@@ -0,0 +1,22 @@
+num1 = 55
+num2 = 5.3
+print(num1)
+print(num2)
+
+languages = ["Python", "Dart", "Web", 23]
+print(languages)
+
+languages = ["Python", "Dart", "Web", 23]
+print(languages[1])
+
+products = ('XBox', 499.99, "Habibi", 23)
+print(products)
+
+products = ('XBox', 499.99, "Habibi", 23)
+print(products[2])
+
+site_name = "Power Learn Project"
+print(site_name)
+
+student_ids = {112, 114, 117, 113}
+print(student_ids)
